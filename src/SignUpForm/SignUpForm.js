53a2c6d72072edd36ce7cd5d34fcdc562bdc384d
DONELL ELLIS <DONELL.ELLIS@GMAIL.COM>
2018-12-27 17:46:12 -0500

    signup form styled

--- src/SignUpForm/SignUpForm.js
+++ src/SignUpForm/SignUpForm.js
@@ -5,21 +5,22 @@ import './SignUpForm.css'
 class SignUpForm extends Component {
   render () {
     return (
-      <div>
-        <h2>Sign Up</h2>
-
-        <form>
-          <div>
-            <label htmlFor='email'>Email</label>
-            <input type='text' name='email' onChange={this.props.handleInput} />
-          </div>
-
-          <div>
-            <label htmlFor='password'>Password</label>
-            <input type='text' name='password' onChange={this.props.handleInput} />
-          </div>
-          <input value='Submit' type='submit' onClick={this.props.handleSignUp} />
-        </form>
+      <div className="home">
+        <div className="home-textbox">
+          <h1 className="home-h1">ancillary</h1>
+          <h2 className="home-h2">a furniture specification and budgeting tool for interior designers</h2>
+          <h3 className="signUp-h3">Sign Up</h3>
+          <form autocomplete="off">
+            <div>
+              <input className="signUp-input" type='text' name='email' placeholder="email" onChange={this.props.handleInput} />
+            </div>
+            <div>
+              <input className="signUp-input" type='text' name='password' placeholder="password" onChange={this.props.handleInput} />
+            </div>
+            <input className="home-button" value='Submit' type='submit' onClick={this.props.handleSignUp} />
+          </form>
+        </div>
+        <img className="home-img" src="https://i.imgur.com/nL4okEY.png" alt="Wishbone Chair by Carl Hansen"/>
       </div>
     )
   }
