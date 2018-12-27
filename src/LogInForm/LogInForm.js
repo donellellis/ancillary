670ac889da6b0962e71dc527ac515d215f0a87d8
DONELL ELLIS <DONELL.ELLIS@GMAIL.COM>
2018-12-27 17:55:42 -0500

    login form styled

--- src/LogInForm/LogInForm.js
+++ src/LogInForm/LogInForm.js
@@ -5,20 +5,22 @@ import './LogInForm.css'
 class LogInForm extends Component {
   render () {
     return (
-      <div>
-        <h2>Log In</h2>
-
-        <form>
-          <div>
-            <label htmlFor='email'>Email</label>
-            <input type='text' name='email' onChange={this.props.handleInput} />
-          </div>
-          <div>
-            <label htmlFor='password'>Password</label>
-            <input type='text' name='password' onChange={this.props.handleInput} />
-          </div>
-          <input value='Submit' type='submit' onClick={this.props.handleLogIn} />
-        </form>
+      <div className="home">
+        <div className="home-textbox">
+          <h1 className="home-h1">ancillary</h1>
+          <h2 className="home-h2">a furniture specification and budgeting tool for interior designers</h2>
+          <h3 className="logIn-h3">Log In</h3>
+          <form autocomplete="off">
+            <div>
+              <input className="logIn-input" type='text' name='email' placeholder='email' onChange={this.props.handleInput} />
+            </div>
+            <div>
+              <input className="logIn-input" type='text' name='password' placeholder="password" onChange={this.props.handleInput} />
+            </div>
+            <input className="home-button" value='Submit' type='submit' onClick={this.props.handleLogIn} />
+          </form>
+        </div>
+        <img className="home-img" src="https://i.imgur.com/nL4okEY.png" alt="Wishbone Chair by Carl Hansen"/>
       </div>
     )
   }
